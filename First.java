c31faa31f6059f9a8bb60f67c27e92c19cfb711a
GauravTripathi10 <tripathi789gaurav@gmail.com>
2023-02-03 13:04:12 +0530

    This is basic Program of java

---
+++ First.java
@@ -0,0 +1,6 @@
+class First{
+	public static void main(String[] args){
+System.out.println("Hello Dost ki gf");
+System.out.println(args[0]); 		//to print the arguments which is pased during excution time
+}
+}
